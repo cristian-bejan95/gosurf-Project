1981db7662d32769b2c5789fa71ef422e3065f39
Cristian Bejan <cristian.bejan@intranet.posta.md>
2023-07-04 16:47:05 +0300

    Swiper

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,6 +1,28 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
- Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+ Swiper.use([Navigation, Pagination, Autoplay]);
+ const headerSliderSpeed = 1500;
  const swiper = new Swiper('.swiper', {
    slidesPerView: 'auto',
    loop: 'true',
+   navigation: {
+    nextEl: ".slider-arrows__next",
+    prevEl: ".slider-arrows__prev",
+  },
+  speed: headerSliderSpeed,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  on:{
+      init: function () {
+        const paginationBullets = document.querySelectorAll('.header__pag .swiper-pagination-bullet');
+        paginationBullets.forEach(el => {
+          el.innerHTML = `<span class="header__bar"></span>`;
+        });
+      },
+  },
  });
